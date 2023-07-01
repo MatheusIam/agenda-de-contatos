67ef2b44680e2b010eaced4a0110cf2e330f4e9e
Matheus Iam <matheusiam27@gmail.com>
2023-06-30 22:20:00 -0400

    Feito o mapeamento JPA da clase Usuario; Pequena alteração no Readme.md

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Projeto simples, para fazer uma agenda de contatos, que utiliza JSF¹, Spring Boot 3², Hibernate e PostgreSQL.
+Projeto simples, para fazer uma agenda de contatos, que utiliza BootStrap 5¹, Spring Boot 3², Hibernate e PostgreSQL.
 
 ¹: Link do repositório -> Em desenvolvimento
 ²: Do ecosistema Spring Boot, foi utilizado Spring Rest e Spring Data.
--- src/main/java/com/matheus/agendatelefonica/entidades/Usuario.java
+++ src/main/java/com/matheus/agendatelefonica/entidades/Usuario.java
@@ -2,33 +2,40 @@ package com.matheus.agendatelefonica.entidades;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "tb_usuarios")
 public class Usuario implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id; // ex. 288
 	
-	private String nomeUsuario;
-	private String senha;
+	@Column(name = "nome_usuarios")
+	private String nomeUsuario; // ex. fulano
 	
-	@OneToMany
-	private Integer agendaID;
+	@Column(name = "senha")
+	private String senha; // ex. senha123
+	
+	@OneToMany(mappedBy = "usuario")
+	private Set<Agenda> agendass; // ex. 32
 	
 	public Usuario () {}
 	
-	public Usuario(Integer id, String nomeUsuario, String senha, Contato contatos, Integer agendaID) {
+	public Usuario(Integer id, String nomeUsuario, String senha, Set<Agenda> agendass) {
 		super();
 		this.id = id;
 		this.nomeUsuario = nomeUsuario;
 		this.senha = senha;
-		this.agendaID = agendaID;
-//		this.contatos = contatos;
+		this.agenda = agenda;
 	}
 	public Integer getId() {
 		return id;
@@ -49,11 +56,13 @@ public class Usuario implements Serializable{
 		this.senha = senha;
 	}
 
-	public Integer getAgendaID() {
-		return agendaID;
+	public Set<Agenda> getAgenda() {
+		return agenda;
 	}
 
-	public void setAgendaID(Integer agendaID) {
-		this.agendaID = agendaID;
+	@Override
+	public String toString() {
+		return "Usuario [id=" + id + ", nomeUsuario=" + nomeUsuario + ", senha=" + senha + "]";
 	}
+	
 }
